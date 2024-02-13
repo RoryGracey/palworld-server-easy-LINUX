ad48397fb983552b8fb76be74f762a042d0b32eb
BuildTools <40322132@live.napier.ac.uk>
2024-02-13 21:35:27 +0000

    first commit

---
+++ src/main.rs
@@ -0,0 +1,112 @@
+use clap::Parser;
+use std::process::Command;
+use std::process::{self, Output};
+
+#[derive(Parser)]
+struct Cli {
+    option: String,
+}
+
+fn install() {
+    Command::new("apt")
+        .arg("update")
+        .status()
+        .expect("Error apt updating ");
+    Command::new("apt")
+        .arg("dist-upgrade")
+        .status()
+        .expect("Error with dist-upgrade");
+    Command::new("apt")
+        .args(["install", "software-properties-common"])
+        .status()
+        .expect("Error installing software-properties-common");
+
+    let device_check: std::process::Output = Command::new("uname")
+        .arg("-a")
+        .output()
+        .expect("Unable to retrieve device type");
+
+    let mut is_ubuntu = false;
+    let uname_output = String::from_utf8_lossy(&device_check.stdout);
+    if uname_output.contains("Ubuntu") {
+        is_ubuntu = true;
+    } else {
+        is_ubuntu = false;
+    }
+
+    if is_ubuntu {
+        let steam_cmd_install = Command::new("sh")
+            .arg("-c")
+            .arg("sudo apt install -y software-properties-common && sudo apt-add-repository -y main universe restricted multiverse && sudo dpkg --add-architecture i386 && sudo apt update && sudo apt install -y steamcmd")
+            .output()
+            .expect("Unable to install SteamCMD");
+        if steam_cmd_install.status.success() {
+            println!("SteamCMD successfully installed!")
+        } else {
+            println!("Unable to install SteamCMD");
+            process::exit(1);
+        }
+    } else {
+        let steam_cmd_install = Command::new("sh")
+            .arg("-c")
+            .arg("apt install software-properties-common && apt-add-repository non-free && dpkg --add-architecture i386 && apt update && apt install steamcmd")
+            .output()
+            .expect("Unable to install steam CMD");
+        if steam_cmd_install.status.success() {
+            println!("SteamCMD successfully installed!")
+        } else {
+            println!("Unable to install SteamCMD");
+            process::exit(1);
+        }
+    }
+
+    let create_user = Command::new("su")
+        .arg("-c")
+        .arg("apt install sudo && useradd -m steam && passwd steam")
+        .output()
+        .expect("Unable to install sudo and add user.");
+    if create_user.status.success() {
+        println!("Create steam user!")
+    } else {
+        println!("Unable to create steam user :(");
+        process::exit(1);
+    }
+
+    let change_user = Command::new("sudo")
+        .arg("-u")
+        .arg("steam")
+        .arg("-s")
+        .output()
+        .expect("Failed to change user.");
+    if change_user.status.success() {
+        println!("Changed to steam user!");
+    } else {
+        println!("Unable to change to steam user.");
+        process::exit(1);
+    }
+
+    let install_palworld = Command::new("su").arg("-c").arg("cd /home/steam && /usr/games/steamcmd +login anonymous +app_update 2394010 validate +quit").output().expect("Unable to run palworld install.");
+
+    if !install_palworld.status.success() {
+        println!("Couldn't install palworld files.");
+        process::exit(1);
+    }
+
+    let check_install = Command::new("su")
+        .arg("-c")
+        .arg("if test -d /home/steam/.steam ; then clear ; echo 'TRUE'; else clear ; echo 'FALSE'; fi")
+        .output()
+        .expect("Unable to run test comamand.");
+    if String::from_utf8_lossy(&check_install.stdout).contains("FALSE") {
+        println!(".steam folder has had issues. It's likely https://github.com/A1RM4X/HowTo-Palworld/blob/main/README-no.steam.md this issue, but we currently don't support fixed for this automatically.");
+        process::exit(1);
+    }
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    if args.option == "install" {
+        install()
+    }
+}
